a48eadafd4567d7cf5863f102e5b555c0c6ca0bb
tandn164 <kenjikatoji999@gmail.com>
2023-08-21 21:15:03 +0900

    [I] init project

---
+++ VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER
+//
+//  Created by Nguyễn Đức Tân on 21/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
